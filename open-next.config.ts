d644fed3be5ca3a547a7c2b29ad6548d41aabe3f
n20186005-hash <n20186005@gmail.com>
2026-09-07 01:30:17 +0800

    更新

--- open-next.config.ts
+++ open-next.config.ts
@@ -3,6 +3,8 @@ import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 // import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 
 export default defineCloudflareConfig({
+	// Run the raw Next.js build internally, instead of recursing into `npm run build`.
+	buildCommand: "next build",
 	// For best results consider enabling R2 caching
 	// See https://opennext.js.org/cloudflare/caching for more details
 	// incrementalCache: r2IncrementalCache
